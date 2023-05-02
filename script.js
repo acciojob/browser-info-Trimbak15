f0880c830384a9c82c05e466998a2f71a6f9be21
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-02 13:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//get the info of the browser
+let browseName = navigator.appName;
+let version = navigator.appVersion;
+
+// display message
+
+let message = "You are using "+browseName+" version "+version;
+
+const divElement = document.getElementById("browser-info");
+divElement.textContent = message;
